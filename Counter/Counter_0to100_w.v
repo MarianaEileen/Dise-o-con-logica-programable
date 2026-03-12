be070e9ac5a1579963851a2ac27d59cf212e72b3
Mariana Eileen Martinez Mancilla <marianaemtzman@gmail.com>
2026-03-12 11:59:22 -0600

    Update Counter_0to100_w.v

--- Counter/Counter_0to100_w.v
+++ Counter/Counter_0to100_w.v
@@ -1,27 +1,27 @@
 module Counter_0to100_w(
-	input  [9:0] SW,
-	input        MAX10_CLK1_50,
-	output [0:6] HEX0,
-	output [0:6] HEX1,
-	output [0:6] HEX2,
-	output [0:6] HEX3
+input [9:0] SW,
+input MAX10_CLK1_50,
+output [0:6] HEX0,
+output [0:6] HEX1,
+output [0:6] HEX2,
+output [0:6] HEX3
 );
 
-wire [0:6] D_un, D_de, D_ce, D_mil;
+wire [0:6] D_un, D_de, D_ce, D_mi;
 
 imprimir TOP(
-	.clk(MAX10_CLK1_50),
-	.rst(SW[0]),
-	.up_down(SW[1]),
-	.load(SW[2]),
-	.data_in(SW[9:3]),
-	.D_un(D_un),
-	.D_de(D_de),
-	.D_ce(D_ce),
-	.D_mi(D_mi)
+.clk(MAX10_CLK1_50),
+.rst(SW[0]),
+.up_down(SW[1]),
+.load(SW[2]),
+.data_in(SW[9:3]),
+.D_un(D_un),
+.D_de(D_de),
+.D_ce(D_ce),
+.D_mi(D_mi)
 );
 
-// concatenacion al orden del display fisico
+// al display
 assign HEX0 = {1'b1, D_un};
 assign HEX1 = {1'b1, D_de};
 assign HEX2 = {1'b1, D_ce};
