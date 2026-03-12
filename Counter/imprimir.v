e7586160e9bf1142fcc1637e43c1e268f45611f9
Mariana Eileen Martinez Mancilla <marianaemtzman@gmail.com>
2026-03-12 12:01:23 -0600

    Update imprimir.v

--- Counter/imprimir.v
+++ Counter/imprimir.v
@@ -1,34 +1,3 @@
-/*module imprimir(
-	input clk, rst,
-	output [6:0] D_un, D_de, D_ce, D_mil
-);
-
-wire reloj;
-wire [13:0] numero;
-
-clk_divider contador(
-	.clk(clk),
-	.rst(rst),
-	.clk_div(reloj)
-);
-
-cero_a_cien cont(
-	.clk(reloj),
-	.rst(rst),
-	.numero(numero)
-);
-	
-BCD_4displays IMP(
-	.bcd_in(numero),
-	.D_un(D_un), 
-	.D_de(D_de),
-	.D_ce(D_ce), 
-	.D_mil(D_mi)
-);
-
-endmodule
-*/
-
 module imprimir(
 	input clk,
 	input rst,
