c092dcd6194b3958925f4311f2253090231d7421
Mariana Eileen Martinez Mancilla <marianaemtzman@gmail.com>
2026-03-12 12:17:39 -0600

    Update Counter_0to100_tb.v
    Updated testbench for Counter_0to100 with additional scenarios and improved reset handling.

--- Counter/Counter_0to100_tb.v
+++ Counter/Counter_0to100_tb.v
@@ -24,21 +24,61 @@ initial
 initial
 	begin
 		// iniciales
+		clk=0;
+		rst=0;
+		up_down=0;
+		load=0;
+		data_in=0;
+
 		rst = 1;
-		load = 0;
-		up_down = 0;
-		data_in = 0;
-		#20;
-		//reset
+		#5;
 		rst = 0;
-		// contador normal up
+		
+		#1100;
+// reset
+		rst=1;
 		#50;
-		// down
-		up_down = 1;
-		#100;
-		$stop;
-		$finish;
+		rst=0;
+		
+		#500;
+		
+// up_down
+		up_down=1;
+		#1100;
+		up_down=0;
+		
+		#500;
+		
+// load
+		load=1;
+		#50;
+		
+		data_in=20;
+		#50;
+// load desactivado
+		load=0;
+		
+		#1000;
+		
+		load=1;
+		#50;
+
+		data_in=40;
+		#50;
+		
+// up_down
+		up_down=1;
+		#50;
+//reset
+		rst=1;
+		#50;
+		rst=0;
+		
+		#50;
+		load=0;
+		#1000;
 	end
 	
 endmodule
+
 
