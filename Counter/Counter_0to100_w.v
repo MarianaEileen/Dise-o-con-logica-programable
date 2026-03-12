e3965a2fbbedf974cfa3c01db9a0f12c4825907d
Mariana Eileen Martinez Mancilla <marianaemtzman@gmail.com>
2026-03-12 12:51:54 -0600

    Update Counter_0to100_w.v

--- Counter/Counter_0to100_w.v
+++ Counter/Counter_0to100_w.v
@@ -1,6 +1,7 @@
 module Counter_0to100_w(
 input [9:0] SW,
 input MAX10_CLK1_50,
+input [1:0] KEY,
 output [0:6] HEX0,
 output [0:6] HEX1,
 output [0:6] HEX2,
@@ -11,17 +12,17 @@ wire [0:6] D_un, D_de, D_ce, D_mi;
 
 imprimir TOP(
 .clk(MAX10_CLK1_50),
-.rst(SW[0]),
-.up_down(SW[1]),
-.load(SW[2]),
-.data_in(SW[9:3]),
+.rst(~KEY[0]),
+.up_down(SW[0]),
+.load(SW[1]),
+.data_in(SW[8:2]),
 .D_un(D_un),
 .D_de(D_de),
 .D_ce(D_ce),
 .D_mi(D_mi)
 );
 
-// al display
+// concatenacion al display fisico
 assign HEX0 = {1'b1, D_un};
 assign HEX1 = {1'b1, D_de};
 assign HEX2 = {1'b1, D_ce};
